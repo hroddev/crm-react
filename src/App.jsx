7051b7f7eae3e4503b60d08d869943bc0c717931
Hector Rodriguez <hector@hrod.dev>
2022-05-19 10:02:21 -0500

    install react router, add pages and layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import { useState } from 'react'
+import { Route, Routes, BrowserRouter }  from 'react-router-dom'
+import Layout from './layout/Layout'
+import Home from './pages/Home'
+import NewClient from './pages/NewClient'
+import EditClient from './pages/EditClient'
 
 function App() {
 
   return (
-    <h1>CRM React</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/clientes' element={<Layout />}>
+            <Route index element={<Home />}/>
+            <Route path='nuevo' element={<NewClient/>} />
+            <Route path='editar/:id' element={<EditClient/>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/LoginForm.jsx
@@ -0,0 +1,10 @@
+
+const LoginForm = () => {
+  return (
+    <div>
+      <h1>Formulario de inicio</h1>
+    </div>
+  )
+}
+
+export default LoginForm
