d233c9005bcffa30f6d1b7baa85e279e4d815f63
Hector Rodriguez <hector@hrod.dev>
2022-05-22 01:11:03 -0500

    add show client

--- src/pages/ShowClient.jsx
+++ src/pages/ShowClient.jsx
@@ -6,8 +6,10 @@ const ShowClient = () => {
     const {id}= useParams()
 
     const [client, setClient] = useState({})
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
+        setLoading(!loading)
         const getClient = async () => {
             try {
                 const url = `http://localhost:4000/clientes/${id}`
@@ -17,14 +19,44 @@ const ShowClient = () => {
             } catch (error) {
                 console.log(error)
             }
+            setLoading(!loading)
         }
         getClient()
     }, [])
 
         return (
-        <div>
-            <h2>Ver Cliente</h2>
-        </div>
+            // validate if the id exist
+            Object.keys(client).length === 0 ? <p>No hay resultados</p> : (
+                <div>
+                    {loading ? 'cargando...' : (
+                    <>
+                        <h1 className='font-black text-blue-900 text-4xl mb-3'>Ver Cliente: {client.name}</h1>
+                        <p>Imformación del Cliente</p>
+
+                        <p className="text-2xl text-gray-700 mt-10">
+                            <span className="uppercase font-bold">Cliente: </span>
+                            {client.name}
+                        </p>
+                        <p className="text-2xl text-gray-700 mt-4">
+                            <span className="uppercase font-bold">Email: </span>
+                            {client.email}
+                        </p>
+                        {client.phone && (
+                            <p className="text-2xl text-gray-700 mt-4">
+                            <span className="uppercase font-bold">Telefono: </span>
+                            {client.phone}
+                        </p>
+                        )}
+                        {client.notes && (
+                            <p className="text-2xl text-gray-700 mt-4">
+                                <span className="uppercase font-bold">Notas: </span>
+                                {client.notes}
+                            </p>
+                        )}
+                    </>
+                    )}
+                </div>
+            )
     )
 }
 
